92a626c95d039e0288306a282dd0cdcb3f16ad9b
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-28 21:49:54 +0800

    update

--- hello/Hello.go
+++ hello/Hello.go
@@ -1,6 +1,15 @@
-package main
+package hello
 
-func main() {
-	print("Hello World")
+import "fmt"
 
+func hello1() {
+	fmt.Printf("Hello World")
+
+}
+func hello2()  {
+	fmt.Printf("hello")
+}
+func Hello()  {
+	hello1()
+	hello2()
 }
---
+++ hello/demo/lissajous/test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"hello"
+)
+
+func main() {
+	fmt.Printf("hello")
+	hello.Hello()
+}
+
