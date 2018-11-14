905a390abf9cdb41284d953b64de0b4f39868a01
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-14 16:29:24 +0800

    update

--- src/go-learning/examples/map/mapTest.go
+++ src/go-learning/examples/map/mapTest.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	ages := make(map[string]int) //mapping from strings to ints
+	ages["shuai"] = 32
+	ages["junlan"] = 232
+	fmt.Println(ages)
+	ages1 := map[string]int{
+		"shuai": 32,
+		"junlan": 232,
+	}
+	delete(ages1, "shuai")
+	fmt.Println(ages1)
+
+	for name, age := range ages{
+		fmt.Printf("%s %d \n", name, age)
+	}
+
+
 }
