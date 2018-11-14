4dc61004f95a954afe5abce112d4767934261230
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-14 19:14:58 +0800

    update

--- src/go-learning/examples/reflect/reflectTest.go
+++ src/go-learning/examples/reflect/reflectTest.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+//http://www.flysnow.org/2017/06/13/go-in-action-go-reflect.html
 func main() {
-	$END$
+	u := User{"shuaijunlan", 23}
+	t := reflect.TypeOf(u)
+	fmt.Println(t)
+
+	fmt.Println(t.Kind())
+
+	v := reflect.ValueOf(u)
+	fmt.Println(v)
+
+	t1 := v.Type()
+	fmt.Println(t1)
+
+	u1 := v.Interface().(User)
+	fmt.Println(u1)
+
+	mPrint := v.MethodByName("Print")
+	args := []reflect.Value{reflect.ValueOf("前缀")}
+	fmt.Println(mPrint.Call(args))
+
+
+	u.Print("hello")
+}
+
+func (u User)Print(prefix string) {
+	fmt.Printf("%s:Name is %s, Age is %d", prefix, u.Name, u.Age)
 }
