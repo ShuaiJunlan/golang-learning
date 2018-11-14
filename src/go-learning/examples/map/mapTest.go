99b509bedb7ffd6307913b223d032b470fa8d679
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-14 16:34:24 +0800

    updatge

--- src/go-learning/examples/map/mapTest.go
+++ src/go-learning/examples/map/mapTest.go
@@ -20,3 +20,17 @@ func main() {
 
 
 }
+
+var graph = make(map[string]map[string]bool)
+
+func addEdge(from, to string) bool {
+	edges := graph[from]
+	if edges != nil {
+		edges = make(map[string]bool)
+		graph[from] = edges
+	}
+	edges[to] = true
+}
+func hasEdge(from, to string) bool {
+	return graph[from][to]
+}
