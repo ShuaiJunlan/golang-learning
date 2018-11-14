aefa8fb95ddd3a8d8e4c708b9ee46846dcafd531
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-14 16:46:06 +0800

    update

--- src/go-learning/simple-web/beego/beegoTest.go
+++ src/go-learning/simple-web/beego/beegoTest.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/astaxie/beego"
 
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Ctx.WriteString("Hello World")
+}
 func main() {
+	beego.Router("/", &MainController{})
 	beego.Run()
 }
