24890dfed38271fbf2a3f52fd3ef2ec7b5f8ee57
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-17 11:46:29 +0800

    udpate

--- src/go-learning/examples/channel/channelbase1_test.go
+++ src/go-learning/examples/channel/channelbase1_test.go
@@ -44,6 +44,44 @@ func TestChannelSync(t *testing.T) {
 	<-syncChan2
 }
 
+func TestChannelSync2(t *testing.T) {
+	var strChan = make(chan string, 3)
+	syncChan1 := make(chan struct{}, 1)
+	syncChan2 := make(chan struct{}, 2)
+	go receive(strChan, syncChan1, syncChan2)
+	go send(strChan, syncChan1, syncChan2)
+	<-syncChan2
+	<-syncChan2
+}
+func receive(strChan <-chan string, syncChan1 <-chan struct{}, syncChan2 chan<- struct{}) {
+	<-syncChan1
+	fmt.Println("Received a sync signal and wait a second ... [receiver]")
+	time.Sleep(time.Second)
+	for true {
+		if elem, ok := <-strChan; ok {
+			fmt.Println("Received:", elem, "[receiver]")
+		} else {
+			break
+		}
+	}
+	fmt.Println("Stopped. [receiver]")
+	syncChan2 <- struct{}{}
+}
+func send(strChan chan<- string, syncChan1 chan<- struct{}, syncChan2 chan<- struct{}) {
+	for _, elem := range []string{"a", "b", "c", "d"} {
+		strChan <- elem
+		fmt.Println("Sent:", elem, "[sender]")
+		if elem == "c" {
+			syncChan1 <- struct{}{}
+			fmt.Println("Send a sync signal. [sender]")
+		}
+	}
+	fmt.Println("Waie 2 seconds... [sender]")
+	time.Sleep(time.Second * 2)
+	close(strChan)
+	syncChan2 <- struct{}{}
+}
+
 var mapChan = make(chan map[string]int, 1)
 
 func TestChannelMap(t *testing.T) {
