c9caffef7c42d967055545a35a91f5aa88b9946a
Junlan Shuai <shuaijunlan@gmail.com>
2018-11-18 19:26:44 +0800

    update

---
+++ src/go-learning/examples/lock/mutexlocak_test.go
@@ -0,0 +1,45 @@
+package lock
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T){
+	var mutex sync.Mutex
+	fmt.Println("Lock the lock. (main)")
+	mutex.Lock()
+	fmt.Println("The lock is locked. (main)")
+	for i := 1; i <= 3; i++ {
+		go func(i int) {
+			fmt.Printf("Lock the lock. (g%d)\n", i)
+			mutex.Lock()
+			fmt.Printf("The lock is locked. (g%d)\n", i)
+		}(i)
+	}
+	time.Sleep(time.Second)
+	fmt.Println("Unlock the lock. (main)")
+	mutex.Unlock()
+	fmt.Println("The lock is unlocked. (main)")
+	time.Sleep(time.Second)
+}
+func TestReadWriteLock(t *testing.T) {
+	var rwm sync.RWMutex
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			fmt.Printf("Try to lock for reading... [%d]\n", i)
+			rwm.RLock()
+			fmt.Printf("Locked for reading. [%d]\n", i)
+			time.Sleep(time.Second * 2)
+			fmt.Printf("Try to unlock for reading... [%d]\n", i)
+			rwm.RUnlock()
+			fmt.Printf("Unlock for reading. [%d]\n", i)
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Try to lock for writing...")
+	rwm.Lock()
+	fmt.Println("Locked for writing.")
+}
