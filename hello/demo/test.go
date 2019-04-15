fe34c3287cdc6e966a8e869b291a997cb40d5ffb
Junlan Shuai <shuaijunlan@gmail.com>
2019-04-15 15:57:37 +0800

    update

--- hello/demo/test.go
+++ hello/demo/test.go
@@ -2,16 +2,20 @@
  * @Author: Shuai Junlan
  * @LastEditors: Shuai Junlan
  * @Date: 2019-04-13 20:38:07
- * @LastEditTime: 2019-04-13 20:38:12
+ * @LastEditTime: 2019-04-15 15:54:53
  */
-package main
+package main // import "github.com/shuaijunlan/golang-learning/hello"
 
 import (
 	"fmt"
 	"hello"
+
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Printf("hello")
 	hello.Hello()
+
+	fmt.Println(quote.Hello())
 }
